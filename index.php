4c3b8c339fe567da70357f0c059eed025e41d242
masashi-js <otaki@postscape.jp>
2017-05-09 18:16:16 +0900

    no message

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<!--[if lt IE 9]><html lang="ja" class="ie ie8"><![endif]-->
+<!--[if IE 9]><html lang="ja" class="ie ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="ja"><!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title></title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="stylesheet" type="text/css" href="css/init.css"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5shiv.min.js"></script>
+<script type="text/javascript" src="js/selectivizr-min.js"></script>
+<![endif]-->
+</head>
+<body>
+	<div id="wrapper">
+
+		<div class="inner">
+
+			<?php
+
+
+			?>
+
+			<script>
+				
+			</script>
+
+
+			
+		</div>
+	
+	</div><!-- Component:#wrapper --> 
+
+
+<!--[if lt IE 9]>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<![endif]--> 
+<!--[if gte IE 9]><!--> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> 
+<!--<![endif]--> 
+<script type="text/javascript" src="js/function.js"></script>
+</body>
+</html>
